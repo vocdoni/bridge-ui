c0a76d9cf4fafd24de9393740d3570ab8f41f591
Jør∂¡ <itsbrickpop@gmail.com>
2021-01-14 17:29:53 +0100

    Proper date estimation text

--- lib/date.ts
+++ lib/date.ts
@@ -1,36 +1,37 @@
 export function strDateDiff(target: Date): string {
     if (!target) return ""
-    const diff = target.getTime() - Date.now()
+    let diff = (target.getTime() - Date.now()) / 1000
     let num: number
 
-    if (diff > 3000) {
+    if (diff > 3) {
         // future
-        if (diff > 1000 * 60 * 60 * 24) {
-            num = Math.floor(diff / 1000 * 60 * 60 * 24)
+        if (diff > 60 * 60 * 24) {
+            num = Math.floor(diff / 60 / 60 / 24)
             return num > 1 ? `in ${num} days` : "in 1 day"
-        } else if (diff > 1000 * 60 * 60) {
-            num = Math.floor(diff / 1000 * 60 * 60)
+        } else if (diff > 60 * 60) {
+            num = Math.floor(diff / 60 / 60)
             return num > 1 ? `in ${num} hours` : "in 1 hour"
-        } else if (diff > 1000 * 60) {
-            num = Math.floor(diff / 1000 * 60)
+        } else if (diff > 60) {
+            num = Math.floor(diff / 60)
             return num > 1 ? `in ${num} minutes` : "in 1 minute"
         } else {
-            num = Math.floor(diff / 1000)
+            num = Math.floor(diff)
             return `in ${num} seconds`
         }
-    } else if (diff < -3000) {
+    } else if (diff < -3) {
+        diff = -diff
         // past
-        if (diff > 1000 * 60 * 60 * 24) {
-            num = Math.floor(diff / 1000 * 60 * 60 * 24)
+        if (diff > 60 * 60 * 24) {
+            num = Math.floor(diff / 60 / 60 / 24)
             return num > 1 ? `${num} days ago` : "1 day ago"
-        } else if (diff > 1000 * 60 * 60) {
-            num = Math.floor(diff / 1000 * 60 * 60)
+        } else if (diff > 60 * 60) {
+            num = Math.floor(diff / 60 / 60)
             return num > 1 ? `${num} hours ago` : "1 hour ago"
-        } else if (diff > 1000 * 60) {
-            num = Math.floor(diff / 1000 * 60)
+        } else if (diff > 60) {
+            num = Math.floor(diff / 60)
             return num > 1 ? `${num} minutes ago` : "1 minute ago"
         } else {
-            num = Math.floor(diff / 1000)
+            num = Math.floor(diff)
             return `${num} seconds ago`
         }
     } else {
