d6944682541fe4df7c97ff1e34e55b18ca2b7dda
Jør∂¡ <itsbrickpop@gmail.com>
2021-01-22 20:16:56 +0100

    Adding the AVT for Goerli

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -3,6 +3,7 @@ import { Token } from "./types"
 // GOERLI
 export const allTokens: Token[] = [
     { symbol: 'VOCB', name: 'Vocdoni Bridge Test', address: "0x2b7222146a805bba0dbb61869c4b3a03209dffba", balanceMappingPosition: 4 },
+    { symbol: 'AVT', name: 'Aragon Vocdoni Test', address: "0xf7a702d8f197e6d510eaf740998c2029744078b5", balanceMappingPosition: 4 },
 ]
 
 export const featuredTokens = allTokens
