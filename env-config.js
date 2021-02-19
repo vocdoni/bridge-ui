fc74969acbccc5ce4787d470d093943d7ef8952e
jordipainan <jordi.pinyana.paga@gmail.com>
2021-02-19 19:13:48 +0000

    add stg environment

--- env-config.js
+++ env-config.js
@@ -12,12 +12,13 @@ module.exports = {
     ETH_NETWORK_ID: process.env.ETH_NETWORK_ID || "goerli",
     ETH_CHAIN_ID: process.env.ETH_CHAIN_ID ? parseInt(process.env.ETH_CHAIN_ID) : 5,
     ETHERSCAN_PREFIX: process.env.ETH_CHAIN_ID || "https://goerli.etherscan.io",
+    ETH_NETWORK_ENVIRONMENT: process.env.ETH_NETWORK_ENVIRONMENT || "dev",
 
     // VOCHAIN
     BLOCK_TIME: process.env.BLOCK_TIME || "12",
 
     // GATEWAYS
-    BOOTNODES_URL: DEVELOPMENT ? "https://bootnodes.vocdoni.net/gateways.dev.json" : (process.env.BOOTNODES_URL || "https://bootnodes.vocdoni.net/gateways.json"),
+    BOOTNODES_URL: process.env.BOOTNODES_URL || "https://bootnodes.vocdoni.net/gateways.dev.json",
 }
 
 console.log("Building the frontend with ENV:", module.exports)
