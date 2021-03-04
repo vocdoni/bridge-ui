fa5f10b35761bdef7133855f214eaa57b17bbfdb
cbrzn <cesarbrazon10@gmail.com>
2021-03-04 05:11:45 +0100

    feat: add token moved to styled componenets + responsive

--- components/button.tsx
+++ components/button.tsx
@@ -1,19 +1,38 @@
-import Link from "next/link"
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
 
-type Props = {
-    children: any,
-    href?: string,
-    onClick?: () => any
+const StyledButton = styled.div`
+    box-sizing: border-box;
+    border-radius: 40px;
+    padding: 15px;
+    cursor: pointer;
+    text-align: center;
+    font-size: 16px;
+    letter-spacing: 0.01em;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+`;
+
+type ButtonProps = {
+    children: React.ReactNode;
+    href?: string;
+    onClick?: () => void;
+};
+
+function Button({ children, onClick, href }: ButtonProps) {
+    if (onClick)
+        return <StyledButton onClick={onClick}>{children}</StyledButton>;
+
+    return (
+        <Link href={href}>
+            <StyledButton>{children}</StyledButton>
+        </Link>
+    );
 }
 
-export default function Button({ children, ...props }: Props) {
-    if (props.onClick) return <div className="button accent-2" onClick={() => props.onClick()}>
-        {children}
-    </div>
-
-    return <Link href={props.href}>
-        <div className="button accent-2">
-            {children}
-        </div>
-    </Link>
-}
+Button.Styled = StyledButton;
+export default Button;
