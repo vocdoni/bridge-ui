30b4a2b2db082c956a7324dabb845ee64467293b
cbrzn <cesarbrazon10@gmail.com>
2021-03-26 14:24:39 +0100

    fix: connector properties passed as expected

--- lib/wallets.ts
+++ lib/wallets.ts
@@ -55,7 +55,7 @@ export const WALLETS: ConnectorData[] = [
 
 export const getConnectors = () => {
     const connectors = {};
-    WALLETS.forEach(({ connector, ...properties }) => {
+    WALLETS.forEach(({ connector, properties }) => {
         connectors[connector] = properties || {};
     });
     return connectors;
