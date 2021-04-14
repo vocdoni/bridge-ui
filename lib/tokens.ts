38dd90379afef6ac9d0f9562099914f160a12be6
p4u <pau@dabax.net>
2021-04-14 23:47:56 +0200

    add rinkeby featured tokens
    Signed-off-by: p4u <pau@dabax.net>

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -4,5 +4,11 @@ export const featuredTokens = {
         "0x2b7222146a805bba0dbb61869c4b3a03209dffba", // VOCB
         "0xf7a702d8f197e6d510eaf740998c2029744078b5", // AVT
     ],
-    rinkeby: []
+    rinkeby: [
+	    "0xca0ea2002a4177f9eb1822092ee0b4c183d91bba", // AVT
+	    "0xc778417e063141139fce010982780140aa0cd5ab", // WETH uniswap
+	    "0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea", // DAI
+	    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+	    "0x0d5263b7969144a852d58505602f630f9b20239d", // ANT
+    ]
 }
