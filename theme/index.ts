2e5f8686f87c3a624b416fde533222c2f20ece0b
cbrzn <cesarbrazon10@gmail.com>
2021-04-23 19:42:06 +0200

    chore: reformat theme file

--- theme/index.ts
+++ theme/index.ts
@@ -199,7 +199,6 @@ export const theme: DefaultTheme = {
 };
 
 export const FixedGlobalStyle = createGlobalStyle`
-
 body {
 	padding: 0;
 	margin: 0;
@@ -208,11 +207,9 @@ body {
 	background-color: ${({ theme }) => theme.white} !important;
 	color: ${({ theme }) => theme.mainText};
 }
-
 h1, h2, h3, h4, h5, h6 {
 	letter-spacing: 0.01em;
 }
-
 h1 { 
 	font-size: 45px; 
 	font-weight: 900; 
@@ -221,7 +218,6 @@ h1 {
 		font-weight: 800; 
 	}
 }
-
 h2 { 
 	font-size: 30px; 
 	font-weight: 900; 
@@ -230,7 +226,6 @@ h2 {
 		font-weight: 700; 
 	}
 }
-
 h3 { 
 	font-size: 20px; 
 	font-weight: 900; 
@@ -239,7 +234,6 @@ h3 {
 		font-weight: 700; 
 	}
 }
-
 h4 { 
 	font-size: 16px; 
 	font-weight: 500; 
@@ -247,35 +241,28 @@ h4 {
 		font-size: 15px; 
 	}
 }
-
 h5 { 
 	font-size: 14px; 
 	font-weight: 500; 
 }
-
 h6 { 
 	font-size: 13px; 
 	font-weight: 500; 
 }
-
 p { 
 	font-size: 14px;
 	@media ${({ theme }) => theme.screens.tablet} {
 			font-size: 12px; 
 	}
 }
-
 a {
 	@media ${({ theme }) => theme.screens.tablet} {
 		font-size: 12px; 
 	}
 }
-
-
 textarea {
 	min-height: 72px;
 }
-
 @font-face {
 	font-family: 'Roboto';
 	font-style: normal;
@@ -300,7 +287,6 @@ textarea {
 	src: local('Roboto Black'), local('Roboto-Black'), url(/fonts/roboto-black.woff2) format('woff2');
 	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
-
 @font-face {
   font-family: 'Overpass';
   font-style: normal;
@@ -309,7 +295,6 @@ textarea {
   src: url(https://fonts.gstatic.com/s/overpass/v5/qFdA35WCmI96Ajtm81kOcc7D4hoiiVI6DLE.woff2) format('woff2');
   unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
 }
-
 @font-face {
   font-family: 'Overpass';
   font-style: normal;
@@ -318,12 +303,10 @@ textarea {
   src: url(https://fonts.gstatic.com/s/overpass/v5/qFdA35WCmI96Ajtm81kOcc7N4hoiiVI6.woff2) format('woff2');
   unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
-
 `;
 
 // New Theme Style | TODO: Modify screens sizes
 export const NewThemeFixedGlobalStyle = createGlobalStyle`
-
 body {
     padding: 0;
     margin: 0;
@@ -332,11 +315,9 @@ body {
     background-color: ${({ theme }) => theme.white} !important;
     color: ${({ theme }) => theme.mainText};
 }
-
 h1, h2, h3, h4, h5, h6 {
     letter-spacing: 0.01em;
 }
-
 h1 { 
     font-size: 74px; 
     font-weight: 700; 
@@ -345,7 +326,6 @@ h1 {
         font-weight: 800; 
     }
 }
-
 h2 { 
     font-size: 64px; 
     font-weight: 700; 
@@ -354,7 +334,6 @@ h2 {
         font-weight: 700; 
     }
 }
-
 h3 { 
     font-size: 54px; 
     font-weight: 600; 
@@ -363,7 +342,6 @@ h3 {
         font-weight: 700; 
     }
 }
-
 h4 { 
     font-size: 44px; 
     font-weight: 500; 
@@ -371,17 +349,14 @@ h4 {
         font-size: 15px; 
     }
 }
-
 h5 { 
     font-size: 34px; 
     font-weight: 500; 
 }
-
 h6 { 
     font-size: 24px; 
     font-weight: 500; 
 }
-
 p { 
     font-size: 20px;
     @media ${({ theme }) => theme.screens.tablet} {
@@ -389,18 +364,14 @@ p {
     }
     
 }
-
 a {
     @media ${({ theme }) => theme.screens.tablet} {
         font-size: 12px; 
     }
 }
-
-
 textarea {
     min-height: 72px;
 }
-
 @font-face {
     font-family: 'Manrope', sans-serif;
     font-style: normal;
@@ -441,5 +412,4 @@ textarea {
     src: url(https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap) format('woff2');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
 }
-
 `;
