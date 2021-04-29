c71a4ba10d45366963af06f6153f4591d4b41b6e
Nestor Barboza <nestorbarboza@gmail.com>
2021-04-29 13:22:52 -0400

    chore: lint

--- components/searchWidget.tsx
+++ components/searchWidget.tsx
@@ -17,7 +17,12 @@ type SearchWidgetProps = {
 const SearchWidget = ({ onChange, onKeyDown, onClick }: SearchWidgetProps) => {
   return (
     <SearchRow>
-      <TextInput placeholder="ERC Token address..." onKeyDown={onKeyDown} onChange={onChange} widthValue={664}/>
+      <TextInput
+        placeholder="ERC Token address..."
+        onKeyDown={onKeyDown}
+        onChange={onChange}
+        widthValue={664}
+      />
       <PrimaryButton onClick={onClick}>Check Token</PrimaryButton>
     </SearchRow>
   );
