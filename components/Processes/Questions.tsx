3a4a987bdb7e0f8e4ca93734136e2519550730bb
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-05-10 08:32:22 +0200

    added vote percentage logic back

--- components/Processes/Questions.tsx
+++ components/Processes/Questions.tsx
@@ -19,32 +19,62 @@ import Checkbox from "./checkbox";
 import { ProcessMetadata } from "dvote-js";
 import { ProcessResults } from "../../lib/hooks/process/useProcessResults";
 
-type PQuestion = ProcessMetadata["questions"][0]
-type PChoice = ProcessMetadata["questions"][0]["choices"][0]
-type PResult = ProcessResults[0]["choices"][0]
+type PQuestion = ProcessMetadata["questions"][0];
+type PChoice = ProcessMetadata["questions"][0]["choices"][0];
+type PResult = ProcessResults[0]["choices"][0];
 
-const Option = ({ choice, choiceResult, onChoiceSelect, questionIdx, checked, canSelect }:
-  { choice: PChoice, choiceResult: PResult, onChoiceSelect: (qIdx: number, value: number) => void, questionIdx: number, checked: boolean, canSelect: boolean }) => (
+const Option = ({
+  choice,
+  choiceResult,
+  onChoiceSelect,
+  questionIdx,
+  checked,
+  canSelect,
+}: {
+  choice: PChoice;
+  choiceResult: PResult;
+  onChoiceSelect: (qIdx: number, value: number) => void;
+  questionIdx: number;
+  checked: boolean;
+  canSelect: boolean;
+}) => (
   <OptionLabel>
     {canSelect ? (
       <Checkbox checked={checked} onChange={() => onChoiceSelect(questionIdx, choice.value)} />
     ) : (
       <ChoiceInfo>
-        <Percentage>{choiceResult?.percentage === "N/A" ? "Locked" : `${choiceResult?.percentage && choiceResult?.percentage || "0.0"}%`}</Percentage>
+        <Percentage>
+          {choiceResult?.percentage === "N/A"
+            ? "Locked"
+            : choiceResult?.percentage === "0"
+            ? "0.00%"
+            : choiceResult?.percentage === "small"
+            ? "<0.01%"
+            : `${choiceResult?.percentage}%`}
+        </Percentage>
       </ChoiceInfo>
     )}
     <OptionTitleContainer>
       <OptionTitle>{choice.title.default}</OptionTitle>
-      <OptionSubtitle>
-        {choiceResult?.votes || ""}
-      </OptionSubtitle>
+      <OptionSubtitle>{choiceResult?.votes || ""}</OptionSubtitle>
     </OptionTitleContainer>
   </OptionLabel>
 );
 
-export const Questions = ({ questions, results, onChoiceSelect, choicesSelected, canSelect }:
-  { questions: PQuestion[], results: ProcessResults, onChoiceSelect: (qIdx: number, value: number) => void, choicesSelected: number[], canSelect: boolean }) => {
-  if (!questions) return <div />
+export const Questions = ({
+  questions,
+  results,
+  onChoiceSelect,
+  choicesSelected,
+  canSelect,
+}: {
+  questions: PQuestion[];
+  results: ProcessResults;
+  onChoiceSelect: (qIdx: number, value: number) => void;
+  choicesSelected: number[];
+  canSelect: boolean;
+}) => {
+  if (!questions) return <div />;
 
   return (
     <div>
@@ -57,7 +87,7 @@ export const Questions = ({ questions, results, onChoiceSelect, choicesSelected,
           </QuestionInformation>
           <QuestionOptions>
             {choices.map((choice, cIdx) => {
-              const choiceResult = results && results[qIdx] && results[qIdx].choices[cIdx]
+              const choiceResult = results && results[qIdx] && results[qIdx].choices[cIdx];
               return (
                 <Option
                   key={cIdx}
--- lib/hooks/process/useProcessResults.ts
+++ lib/hooks/process/useProcessResults.ts
@@ -1,5 +1,6 @@
 import { ProcessInfo, useBlockStatus, usePool } from "@vocdoni/react-hooks";
 import { VotingApi } from "dvote-js";
+import { BigNumber } from "ethers";
 import { useEffect, useState } from "react";
 import TokenAmount from "token-amount";
 import { TokenInfo } from "../../types";
@@ -11,24 +12,24 @@ export type ProcessResults = {
     votes: string;
     percentage: string;
   }[];
-}[]
+}[];
 
 export const useProcessResults = (processInfo: ProcessInfo, tokenInfo: Partial<TokenInfo>) => {
   const { poolPromise } = usePool();
   const { blockStatus } = useBlockStatus();
-  const [results, setResults] = useState<ProcessResults>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState("")
+  const [results, setResults] = useState<ProcessResults>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-  const processId = processInfo?.id
-  const tokenAddress = tokenInfo?.address
+  const processId = processInfo?.id;
+  const tokenAddress = tokenInfo?.address;
 
   useEffect(() => {
-    const interval = setInterval(() => fetchCurrentResults(), 1000 * 30)
-    fetchCurrentResults()
+    const interval = setInterval(() => fetchCurrentResults(), 1000 * 30);
+    fetchCurrentResults();
 
-    return () => clearInterval(interval)
-  }, [processId, tokenAddress, !!blockStatus])
+    return () => clearInterval(interval);
+  }, [processId, tokenAddress, !!blockStatus]);
 
   // Loader
 
@@ -44,19 +45,17 @@ export const useProcessResults = (processInfo: ProcessInfo, tokenInfo: Partial<T
       if (blockStatus.blockNumber < endBlock) {
         // Return empty results
 
-        const results = processInfo.metadata.questions.map(
-          ({ title, choices }) => {
-            const choicesFormatted = choices.map(({ title: choiceTitle }) => ({
-              title: choiceTitle.default,
-              votes: "",
-              percentage: "N/A",
-            }));
-            return {
-              title: title.default,
-              choices: choicesFormatted,
-            };
-          }
-        );
+        const results = processInfo.metadata.questions.map(({ title, choices }) => {
+          const choicesFormatted = choices.map(({ title: choiceTitle }) => ({
+            title: choiceTitle.default,
+            votes: "",
+            percentage: "N/A",
+          }));
+          return {
+            title: title.default,
+            choices: choicesFormatted,
+          };
+        });
         setResults(results);
         setError("");
         return;
@@ -74,13 +73,32 @@ export const useProcessResults = (processInfo: ProcessInfo, tokenInfo: Partial<T
       // for different type of voting
 
       const results = response.questions.map(({ title, voteResults }) => {
+        const totalVoteAmountBn = voteResults.reduce((prev: BigNumber, { title, votes }) => {
+          return prev.add(votes);
+        }, BigNumber.from(0));
         const choices = voteResults.map(({ title, votes }) => {
-          const percentage = votes.mul(100 * 10).div(tokenInfo.totalSupply).toNumber();
+          let percentage = "0";
+          if (!votes.isZero()) {
+            // Since BigNumber only do rounded division, it is necessary necessary to
+            // multiply the votes by 100 before the division. However, this will yield
+            // integer percentages. In order to get percentages with 2 decimal places,
+            // it is necessary to multiply the votes by an additional 100 before the
+            // division
+            const factorBn = votes.mul(10000).div(totalVoteAmountBn);
+            // factor is now within [0,10'000]
+
+            if (factorBn.isZero()) {
+              percentage = "small"; //vote is too small for percentage with 2 decimals
+            } else {
+              percentage = (factorBn.toNumber() / 100).toFixed(2);
+              //percentage is now within [0,100], rounded to 2 decimal places.
+            }
+          }
           const vote = new TokenAmount(votes, tokenInfo.decimals);
           return {
             title: title.default,
             votes: `${vote.toString()} ${tokenInfo.symbol}`,
-            percentage: Number(percentage / 10).toFixed(1),
+            percentage: percentage,
           };
         });
         return {
@@ -95,19 +113,17 @@ export const useProcessResults = (processInfo: ProcessInfo, tokenInfo: Partial<T
       setLoading(false);
 
       if (e?.message == "The results are not available") {
-        const results: ProcessResults = processInfo.metadata.questions.map(
-          ({ title, choices }) => {
-            const choicesFormatted = choices.map(({ title: choiceTitle }) => ({
-              title: choiceTitle.default,
-              votes: hasEncryptedVotes ? "" : "0 " + tokenInfo.symbol,
-              percentage: hasEncryptedVotes ? "" : "0.0%",
-            }));
-            return {
-              title: title.default,
-              choices: choicesFormatted,
-            };
-          }
-        );
+        const results: ProcessResults = processInfo.metadata.questions.map(({ title, choices }) => {
+          const choicesFormatted = choices.map(({ title: choiceTitle }) => ({
+            title: choiceTitle.default,
+            votes: hasEncryptedVotes ? "" : "0 " + tokenInfo.symbol,
+            percentage: hasEncryptedVotes ? "" : "0.0%",
+          }));
+          return {
+            title: title.default,
+            choices: choicesFormatted,
+          };
+        });
         setResults(results);
         setError("");
         return;
