4d7c84c8f32ecfb4f4ce564591f546dc3a264829
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-06-01 17:39:09 +0200

    improved section titles spacing

--- components/sectionTitle.tsx
+++ components/sectionTitle.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const Title = styled.h4`
-  margin-bottom: 5px;
-  font-size: 38px;
+const TitleContainer = styled.div`
+  padding-top: 48px;
+  padding-bottom: 16px;
+`;
+
+const Title = styled.h4<{ hasSubtitle: boolean; isSmaller: boolean }>`
+  margin-top: 0px;
+  ${({ hasSubtitle }) => (hasSubtitle ? "margin-bottom: 8px;" : "margin-bottom: 0px;")}
   font-style: normal;
   font-weight: 500;
-  line-height: 51.91px;
   letter-spacing: -0.03em;
   color: ${({ theme }) => theme.blackAndWhite.b1};
-  margin-top: 45px;
+  ${({ isSmaller }) => (isSmaller ? "font-size: 32px;" : "font-size: 38px;")}
+  ${({ isSmaller }) => (isSmaller ? "line-height: 35.52px;" : "line-height: 51.91px;")}
+  
 
   @media ${({ theme }) => theme.screens.tablet} {
     font-size: 32px;
     margin-top: 30px;
   }
-`;
-
-const SmallerTitle = styled(Title)`
-  font-size: 26px;
-  line-height: 35.52px;
 `;
 
 const Subtitle = styled.p`
@@ -29,19 +30,25 @@ const Subtitle = styled.p`
   font-style: normal;
   font-weight: 400;
   margin-top: 0;
+  margin-bottom: 8px;
 `;
 
 type TitleProps = {
-  smallerTitle?: boolean;
   title: string;
   subtitle?: string;
+  smallerTitle?: boolean;
 };
 
-const SectionTitle = ({ smallerTitle, title, subtitle }: TitleProps) => (
-  <div>
-    {smallerTitle ? <SmallerTitle>{title}</SmallerTitle> : <Title>{title}</Title>}
-    {subtitle ? <Subtitle>{subtitle}</Subtitle> : null}
-  </div>
-);
+const SectionTitle = ({ title, subtitle, smallerTitle }: TitleProps) => {
+  const hasSubtitle = !!subtitle;
+  return (
+    <TitleContainer>
+      <Title hasSubtitle={hasSubtitle} isSmaller={smallerTitle}>
+        {title}
+      </Title>
+      {hasSubtitle ? <Subtitle>{subtitle}</Subtitle> : null}
+    </TitleContainer>
+  );
+};
 
 export default SectionTitle;
