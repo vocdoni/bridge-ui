0042bde6a0df5605e12e65476653631a004305d3
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-06-02 12:35:32 +0200

    refactored token fetching

--- lib/errors.ts
+++ lib/errors.ts
@@ -3,3 +3,4 @@ export const METAMASK_IS_NOT_AVAILABLE = "Metamask is not available";
 export const YOU_ARE_NOT_CONNECTED = "You are not connected";
 export const NO_TOKEN_BALANCE = "You have no token balance";
 export const TOKEN_ALREADY_REGISTERED = "The token is already registered";
+export const TOKEN_ADDRESS_INVALID = "The token address is not valid";
