28552f1fa1be096aa3dbcdfac5a910201e74d7da
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-06-14 19:09:21 +0200

    refactored new proposal format errors

--- lib/errors.ts
+++ lib/errors.ts
@@ -5,3 +5,89 @@ export const NO_TOKEN_BALANCE = "You have no token balance";
 export const TOKEN_ALREADY_REGISTERED = "The token is already registered";
 export const TOKEN_ADDRESS_INVALID = "The token address is not valid";
 export const USER_CANCELED_TX = "The transaction was canceled";
+
+class NoTokenBalanceError extends Error {
+  constructor() {
+    super("You have no token balance");
+  }
+}
+
+// NEW PROPOSAL FORMAT ===================================================================
+
+export enum InputType {
+  TITLE = "title",
+  DESCRIPTION = "description",
+  START_DATE = "start date",
+  END_DATE = "end date",
+}
+
+export class MissingInputError extends Error {
+  constructor(inputType: InputType, index = -1) {
+    if (index < 0) super(`Please enter a ${inputType}`);
+    else super(`Please enter a ${inputType} for question #${index + 1}`);
+  }
+}
+
+export class ShortInputError extends Error {
+  constructor(inputType: InputType, index: number = -1) {
+    if (index < 0) super(`Please enter a longer ${inputType}`);
+    else super(`Please enter a longer ${inputType} for question #${index + 1}`);
+  }
+}
+
+export class LongInputError extends Error {
+  constructor(inputType: InputType, maxLength: number, index: number = -1) {
+    if (index < 0) {
+      super(`The ${inputType} is too long. Please keep it shorter than ${maxLength} characters`);
+    } else {
+      super(
+        `The ${inputType} for question #${
+          index + 1
+        } is too long. Please keep it shorter than ${maxLength} characters`
+      );
+    }
+  }
+}
+
+export class ShortChoiceError extends Error {
+  constructor(choiceIndex, questionIndex) {
+    super(`Please fill text for choice #${choiceIndex + 1} of question #${questionIndex + 1}`);
+  }
+}
+
+export class LongChoiceError extends Error {
+  constructor(choiceIndex, questionIndex, maxLength) {
+    super(
+      `The text for choice #${choiceIndex + 1} of question #${
+        questionIndex + 1
+      } is too long. Please make it shorter than ${maxLength} characters`
+    );
+  }
+}
+
+export enum DateErrorType {
+  EARLY_START,
+  EARLY_END,
+  SMALL_INTERVAL,
+}
+
+export class DateError extends Error {
+  constructor(errorType: DateErrorType) {
+    let errorMsg;
+    switch (errorType) {
+      case DateErrorType.EARLY_START:
+        errorMsg = "The start date must be at least 5 minutes from now";
+        break;
+      case DateErrorType.EARLY_END:
+        errorMsg = "The end date must be at least 10 minutes from now";
+        break;
+      case DateErrorType.SMALL_INTERVAL:
+        errorMsg = "The end date must be at least 5 minutes after the start";
+        break;
+      default:
+        errorMsg = "Date error not properly set.";
+        break;
+    }
+    super(errorMsg);
+  }
+}
--- lib/processValidator.ts
+++ lib/processValidator.ts
@@ -1,42 +1,51 @@
 import { ProcessMetadata } from "dvote-js";
 import moment from "moment";
+import {
+  DateError,
+  DateErrorType,
+  ShortInputError,
+  LongInputError,
+  InputType,
+  MissingInputError,
+  ShortChoiceError,
+  LongChoiceError,
+} from "./errors";
 
 const MAX_TITLE_LENGHT = 2048;
 const MAX_DESCRIPTION_LENGTH = 4096;
 const MAX_QUESTION_TITLE_LENGHT = 512;
 
 export const validateProposal = (proposal: ProcessMetadata, startDate: Date, endDate: Date) => {
-  
-  if (!proposal.title || proposal.title.default.trim().length < 2)
-    throw new Error("Please enter a title");
+  if (!proposal.title) throw new MissingInputError(InputType.TITLE);
 
-  if (!proposal.description || proposal.description.default.trim().length < 2)
-    throw new Error("Please enter a description");
-  
-  const isLongTitle = new Blob([proposal.title.default]).size > MAX_TITLE_LENGHT;
-  if (isLongTitle) {
-    throw new Error(`Please make the title shorter than ${MAX_TITLE_LENGHT} characters`);
-  }
+  const trimmedTitle = proposal.title.default.trim();
+  if (isShortInput(trimmedTitle)) throw new ShortInputError(InputType.TITLE);
 
-  const isBigDescription = new Blob([proposal.description.default]).size > MAX_DESCRIPTION_LENGTH;
-  if (isBigDescription) {
-    throw new Error(`Please make the description shorter than ${MAX_DESCRIPTION_LENGTH} characters`);
-  }
+  if (!proposal.description) throw new MissingInputError(InputType.DESCRIPTION);
 
-  proposal.title.default = proposal.title.default.trim();
-  proposal.description.default = proposal.description.default.trim();
+  const trimmedDescription = proposal.description.default.trim();
+  if (isShortInput(trimmedDescription)) throw new ShortInputError(InputType.DESCRIPTION);
 
-  proposal.questions.map(validateQuestion);
+  const isLongTitle = new Blob([trimmedTitle]).size > MAX_TITLE_LENGHT;
+  if (isLongTitle) throw new LongInputError(InputType.TITLE, MAX_TITLE_LENGHT);
 
-  if (!startDate) throw new Error("Please enter a start date");
-  else if (!endDate) throw new Error("Please enter an ending date");
+  const isBigDescription = new Blob([trimmedDescription]).size > MAX_DESCRIPTION_LENGTH;
+  if (isBigDescription) throw new LongInputError(InputType.DESCRIPTION, MAX_DESCRIPTION_LENGTH);
+
+  proposal.title.default = trimmedTitle;
+  proposal.description.default = trimmedDescription;
+
+  proposal.questions.forEach(validateQuestion);
+
+  if (!startDate) throw new MissingInputError(InputType.START_DATE);
+  if (!endDate) throw new MissingInputError(InputType.END_DATE);
 
   if (moment(startDate).isBefore(moment().add(5, "minutes"))) {
-    throw new Error("The start date must be at least 5 minutes from now");
+    throw new DateError(DateErrorType.EARLY_START);
   } else if (moment(endDate).isBefore(moment().add(10, "minutes"))) {
-    throw new Error("The end date must be at least 10 minutes from now");
+    throw new DateError(DateErrorType.EARLY_END);
   } else if (moment(endDate).isBefore(moment(startDate).add(5, "minutes"))) {
-    throw new Error("The end date must be at least 5 minutes after the start");
+    throw new DateError(DateErrorType.SMALL_INTERVAL);
   }
 
   for (let qIdx = 0; qIdx < proposal.questions.length; qIdx++) {
@@ -46,44 +55,43 @@ export const validateProposal = (proposal: ProcessMetadata, startDate: Date, end
       question.choices[cIdx].value = cIdx;
     }
   }
+};
 
-}
+export const validateQuestion = ({ title, description, choices }, index) => {
+  if (!title) throw new MissingInputError(InputType.TITLE, index);
 
-export const validateQuestion = ({ title, description, choices }, index) => {  
-    
-  if (!title || title.default.trim().length < 2)
-    throw new Error(`Please enter a title for question #${index + 1}`);
+  const trimmedTitle = title.default.trim();
+  if (isShortInput(trimmedTitle)) throw new ShortInputError(InputType.TITLE, index);
 
-  const isLongTitle = new Blob([title.default]).size > MAX_TITLE_LENGHT;
-  if (isLongTitle) {
-    throw new Error(`The title for question #${index + 1} is too long. Please make it shorter than ${MAX_TITLE_LENGHT} characters`);
-  }
+  const isLongTitle = new Blob([trimmedTitle]).size > MAX_TITLE_LENGHT;
+  if (isLongTitle) throw new LongInputError(InputType.TITLE, MAX_TITLE_LENGHT, index);
 
-  title.default = title.default.trim();
+  if (!description) throw new MissingInputError(InputType.DESCRIPTION, index);
 
-  const isBigDescription = new Blob([description.default]).size > MAX_DESCRIPTION_LENGTH;
+  const trimmedDescription = description.default.trim();
+  if (isShortInput(trimmedDescription)) throw new ShortInputError(InputType.DESCRIPTION, index);
+
+  const isBigDescription = new Blob([trimmedDescription]).size > MAX_DESCRIPTION_LENGTH;
   if (isBigDescription) {
-    throw new Error(`The description of question #${index + 1} is too long. Please make it shorter than ${MAX_DESCRIPTION_LENGTH} characters`);
+    throw new LongInputError(InputType.DESCRIPTION, MAX_DESCRIPTION_LENGTH, index);
   }
 
-  if(description && description.default.trim().length >=2) {
-    description.default = description.default.trim()
+  title.default = trimmedTitle;
+  description.default = trimmedDescription;
+
+  let faultyChoice = choices.findIndex(({ title }) => title.default.trim().length < 2);
+  if (faultyChoice >= 0) throw new ShortChoiceError(faultyChoice, index);
+
+  faultyChoice = choices.findIndex(
+    ({ title }) => new Blob([title.default]).size > MAX_QUESTION_TITLE_LENGHT
+  );
+  if (faultyChoice >= 0) {
+    throw new LongChoiceError(faultyChoice, index, MAX_QUESTION_TITLE_LENGHT);
   }
 
-  const choiceTooSmall = choices.findIndex(({ title }) => title.default.trim().length < 2);
-
-  if (choiceTooSmall > -1) {
-    throw new Error(`Please fill text for choice #${choiceTooSmall + 1} of question #${index + 1}`);
-  }
-
-  const choiceTooBig = choices.findIndex(({ title }) => new Blob([title.default]).size > MAX_QUESTION_TITLE_LENGHT);
-
-  if (choiceTooBig > -1) {
-    throw new Error(`The text for choice #${choiceTooBig + 1} of question #${index + 1} is too long. Please make it shorter than ${MAX_QUESTION_TITLE_LENGHT} characters`);
-  }
-
-  choices = choices.forEach(choice => {
+  choices = choices.forEach((choice) => {
     choice.title.default = choice.title.default.trim();
   });
-
 };
+
+const isShortInput = (input: string) => input.length < 2;
