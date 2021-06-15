67b7cd16cfcb1c0c108ca99694222e9902d86da9
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-06-15 10:49:46 +0200

    removed unused code

--- components/cookies-banner.tsx
+++ components/cookies-banner.tsx
@@ -1,73 +1,73 @@
-import React from 'react'
-import Link from 'next/link'
-import styled from 'styled-components'
-import { COOKIES_URL } from '../lib/constants'
+import React from "react";
+import { If } from "react-if";
+import styled from "styled-components";
+
+import { COOKIES_URL } from "../lib/constants";
+import { useCookies } from "../lib/hooks/cookies";
+
+import { size } from "../theme/index";
 import { PrimaryButton, SecondaryButton } from "../components/ControlElements/button";
 
-import { size } from '../theme/index'
-import { useCookies } from '../lib/hooks/cookies'
-import { If } from 'react-if'
-
 export const CookiesBanner = () => {
-  const { acceptCookies, rejectCookies, accepted, hide } = useCookies()
+  const { acceptCookies, rejectCookies, accepted, hide } = useCookies();
 
-  const showCookiesBanner = !accepted && !hide
+  const showCookiesBanner = !accepted && !hide;
   return (
     <If condition={showCookiesBanner}>
       <CookiesContainer>
         <TextContainer>
           <FlexContainer>
             <SpacedContainer>
-              
-                <p>This website uses cookies to improve your experience while browsing the website. The cookies that are classified as necessary are stored in your browser, as they are essential for the functioning of the basic functionalities of the website.</p>
-              
-                <p>We also use third-party cookies that help us analyze and understand how you use this website. These cookies will be stored in your browser only with your consent. You also have the option to opt out of receiving these cookies. But opting out of some of these cookies may affect your browsing experience.{' '}
-                <a href={COOKIES_URL} target="_blank">
+              <p>
+                This website uses cookies to improve your experience while browsing the website. The
+                cookies that are classified as necessary are stored in your browser, as they are
+                essential for the functioning of the basic functionalities of the website.
+              </p>
+
+              <p>
+                We also use third-party cookies that help us analyze and understand how you use this
+                website. These cookies will be stored in your browser only with your consent. You
+                also have the option to opt out of receiving these cookies. But opting out of some
+                of these cookies may affect your browsing experience.{" "}
+                <a href={COOKIES_URL} target="_blank" rel="noreferrer noopener">
                   More details
-                </a></p>
-              
+                </a>
+              </p>
             </SpacedContainer>
 
             <ButtonsContainer>
-
               <SpacedContainer>
-                <PrimaryButton onClick={acceptCookies}>
-                  Accept
-                </PrimaryButton>
+                <PrimaryButton onClick={acceptCookies}>Accept</PrimaryButton>
               </SpacedContainer>
 
               <SpacedContainer>
-                <SecondaryButton onClick={rejectCookies}>
-                  Decline
-                </SecondaryButton>
+                <SecondaryButton onClick={rejectCookies}>Decline</SecondaryButton>
               </SpacedContainer>
-
             </ButtonsContainer>
           </FlexContainer>
         </TextContainer>
       </CookiesContainer>
     </If>
-  )
-}
+  );
+};
 
 const ButtonsContainer = styled.div`
-display: flex;
-flex-direction: row;
-`
+  display: flex;
+  flex-direction: row;
+`;
 
 const FlexContainer = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-around;
-padding: 0 8px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 8px;
 
-
-@media ${({ theme }) => theme.screens.laptop} {
-  flex-direction: column;
-  padding: 0 14px;
-}
-`
+  @media ${({ theme }) => theme.screens.laptop} {
+    flex-direction: column;
+    padding: 0 14px;
+  }
+`;
 
 const TextContainer = styled.div`
   max-width: ${size.laptopL * 0.8}px;
@@ -80,7 +80,7 @@ const TextContainer = styled.div`
   & > div > div {
     padding-right: 10px;
   }
-`
+`;
 const SpacedContainer = styled.div`
   margin-left: 14px;
   margin-top: 0;
@@ -88,7 +88,7 @@ const SpacedContainer = styled.div`
   @media ${({ theme }) => theme.screens.laptop} {
     margin-top: 14px;
   }
-`
+`;
 
 const CookiesContainer = styled.div`
   position: fixed;
@@ -98,4 +98,4 @@ const CookiesContainer = styled.div`
   z-index: 1;
   padding: 10px 0;
   background-color: white;
-`
+`;
--- lib/hooks/cookies.tsx
+++ lib/hooks/cookies.tsx
@@ -1,37 +1,34 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const COOKIES_STORE_KEY = 'cookies-acceptance'
+const COOKIES_STORE_KEY = "cookies-acceptance";
 
 enum CookiesStatus {
-  Accept = 'accept',
-  Reject = 'reject',
+  Accept = "accept",
+  Reject = "reject",
 }
 export function useCookies() {
-  const [accepted, setAccepted] = useState<boolean>(false)
-  const [hide, setHide] = useState<boolean>(false)
-
-  const router = useRouter()
+  const [accepted, setAccepted] = useState<boolean>(false);
+  const [hide, setHide] = useState<boolean>(false);
 
   useEffect(() => {
-    const cookieAcceptance = localStorage.getItem(COOKIES_STORE_KEY)
+    const cookieAcceptance = localStorage.getItem(COOKIES_STORE_KEY);
 
     if (cookieAcceptance) {
-      setAccepted(cookieAcceptance === CookiesStatus.Accept)
+      setAccepted(cookieAcceptance === CookiesStatus.Accept);
     }
-  }, [])
+  }, []);
 
   const acceptCookies = () => {
-    setAccepted(true)
-    setHide(true)
-    localStorage.setItem(COOKIES_STORE_KEY, CookiesStatus.Accept)
-  }
+    setAccepted(true);
+    setHide(true);
+    localStorage.setItem(COOKIES_STORE_KEY, CookiesStatus.Accept);
+  };
 
   const rejectCookies = () => {
-    setAccepted(false)
-    setHide(true)
-    localStorage.setItem(COOKIES_STORE_KEY, CookiesStatus.Reject)
-  }
+    setAccepted(false);
+    setHide(true);
+    localStorage.setItem(COOKIES_STORE_KEY, CookiesStatus.Reject);
+  };
 
-  return { acceptCookies, rejectCookies, accepted, hide }
+  return { acceptCookies, rejectCookies, accepted, hide };
 }
