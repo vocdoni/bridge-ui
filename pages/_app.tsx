dac1f34730dd3ccfde1ae501287fcc680f4c1e75
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-07-22 15:04:26 +0200

    restructuring _app components and introduced network vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
-import React, { FC } from "react";
+import React from "react";
 import { NextComponentType, NextPageContext } from "next";
 import { AppInitialProps } from "next/app";
 import Head from "next/head";
 import { Router } from "next/router";
-import { UseWalletProvider } from "use-wallet";
+import { UseWalletProvider, useWallet } from "use-wallet";
 import { UsePoolProvider, UseProcessProvider, UseBlockStatusProvider } from "@vocdoni/react-hooks";
 import { EthNetworkID, VocdoniEnvironment } from "dvote-js";
 import { ThemeProvider } from "styled-components";
-import "react-datetime/css/react-datetime.css";
 
 import { UseMessageAlertProvider } from "../lib/hooks/message-alert";
 import { UseLoadingAlertProvider } from "../lib/hooks/loading-alert";
@@ -19,6 +18,7 @@ import { FixedGlobalStyle, theme } from "../theme";
 import { Layout } from "../components/StructuralElements/layout";
 import { ModalsProvider } from "../components/Modal/context";
 import { CookiesBanner } from "../components/cookies-banner";
+import { BUILD, getNetworkVars } from "../lib/constants/env";
 
 Router.events.on("routeChangeComplete", (url: string) => trackPage(url));
 
@@ -27,66 +27,73 @@ type NextAppProps = AppInitialProps & {
   router: Router;
 };
 
-const VoiceApp: FC<NextAppProps> = ({ Component, pageProps }) => {
-  const chainId = parseInt(process.env.ETH_CHAIN_ID);
-  const bootnodeUri = process.env.BOOTNODES_URL;
-  const networkId = process.env.ETH_NETWORK_ID as EthNetworkID;
-  const environment = process.env.VOCDONI_ENVIRONMENT as VocdoniEnvironment;
-  const appTitle = process.env.APP_TITLE;
-  const commitSHA = process.env.COMMIT_SHA;
-
+const VoiceApp = ({ Component, router, pageProps }: NextAppProps) => {
   const connectors = getConnectors();
+  console.log("BUILT WITH " + JSON.stringify(BUILD, null, 2));
 
   return (
-    <UseMessageAlertProvider>
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <UseMessageAlertProvider>
         <UseLoadingAlertProvider>
-          <UsePoolProvider
-            bootnodeUri={bootnodeUri}
-            networkId={networkId}
-            environment={environment}
-            minNumGateways={1}
-          >
-            <UseBlockStatusProvider>
-              <UseStoredTokensProvider>
-                <UseProcessProvider>
-                  <UseWalletProvider connectors={connectors || {}}>
-                    <UseTokensWithBalance>
-                      <ModalsProvider>
-                        <FixedGlobalStyle />
-
-                        <Head>
-                          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-                          <link
-                            href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
-                            rel="stylesheet"
-                          ></link>
-                          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
-                          <link
-                            href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
-                            rel="stylesheet"
-                          />
-                          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                          <title>{appTitle}</title>
-                        </Head>
-                        <Layout>
-                          <Component {...pageProps} />
-                        </Layout>
-                        <div id="commit-sha" style={{ display: "none" }}>
-                          {commitSHA}
-                        </div>
-                        <CookiesBanner />
-                      </ModalsProvider>
-                    </UseTokensWithBalance>
-                  </UseWalletProvider>
-                </UseProcessProvider>
-              </UseStoredTokensProvider>
-            </UseBlockStatusProvider>
-          </UsePoolProvider>
+          <UseWalletProvider connectors={connectors || {}}>
+            <AppWithWallet Component={Component} router={router} pageProps={pageProps} />
+          </UseWalletProvider>
         </UseLoadingAlertProvider>
-      </ThemeProvider>
-    </UseMessageAlertProvider>
+      </UseMessageAlertProvider>
+    </ThemeProvider>
   );
 };
+
+const AppWithWallet = ({ Component, pageProps }: NextAppProps) => {
+  const { chainId } = useWallet();
+  const { networkName, bootnodesUrl } = getNetworkVars(chainId);
+  console.log("ENVIRONMENT " + JSON.stringify(getNetworkVars(chainId), null, 2));
+
+  return (
+    <UsePoolProvider
+      bootnodeUri={bootnodesUrl}
+      networkId={networkName as EthNetworkID}
+      environment={BUILD.vocdoniEnvironment as VocdoniEnvironment}
+      minNumGateways={1}
+    >
+      <UseBlockStatusProvider>
+        <UseStoredTokensProvider>
+          <UseProcessProvider>
+            <UseTokensWithBalance>
+              <ModalsProvider>
+                <FixedGlobalStyle />
+                <HtmlHead appTitle={BUILD.appTitle} />
+                <Layout>
+                  <Component {...pageProps} />
+                </Layout>
+                <div id="commit-sha" style={{ display: "none" }}>
+                  {BUILD.commitSha}
+                </div>
+                <CookiesBanner />
+              </ModalsProvider>
+            </UseTokensWithBalance>
+          </UseProcessProvider>
+        </UseStoredTokensProvider>
+      </UseBlockStatusProvider>
+    </UsePoolProvider>
+  );
+};
+
+const HtmlHead = ({ appTitle }) => (
+  <Head>
+    <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    ></link>
+    <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{appTitle}</title>
+  </Head>
+);
 
 export default VoiceApp;
