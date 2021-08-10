fa7a72e559f4fec1b616b6713c546b022f91ebee
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2021-08-10 17:07:37 +0200

    make voice work as it is

--- next.config.js
+++ next.config.js
@@ -3,4 +3,32 @@ const env = require("./env-config.js");
 module.exports = {
   trailingSlash: true,
   env,
+  // TODO: if we use the latest version of useWallet multichain
+  // webpack needs to handle image loadder properly
+  // the bellow commented config do loads the images
+  // but the app still don't work, perhaps the path is not correct
+  // more configuration is needed
+
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+  //     use: [
+  //       options.defaultLoaders.babel,
+  //       {
+  //         loader: "emit-file-loader",
+  //         options: {
+  //           name: "dist/[path][name].[ext]",
+  //         },
+  //       },
+  //       {
+  //         loader: "file-loader",
+  //         options: {
+  //           name: "dist/[path][name].[ext]",
+  //         },
+  //       },
+  //     ],
+  //   });
+
+  //   return config;
+  // },
 };
