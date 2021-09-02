9f625a3003fe5dac655ac2adf8d34f61e39759f7
Jør∂¡ <itsbrickpop@gmail.com>
2021-09-02 12:36:47 +0100

    Upgrading to 1.11.1

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import { BigNumber, Contract, ethers, providers, Signer } from "ethers";
-import { CensusErc20Api, GatewayPool, IProcessSummary, VotingApi } from "dvote-js";
+import { CensusErc20Api, GatewayPool, ProcessSummary, VotingApi } from "dvote-js";
 import TokenAmount from "token-amount";
 import Bluebird from "bluebird";
 import { NoTokenBalanceError, NO_TOKEN_BALANCE } from "./errors";
@@ -175,7 +175,7 @@ export async function waitUntilProcessCreated(
 ): Promise<boolean> {
   let retries = 30;
   while (retries >= 0) {
-    const info: IProcessSummary = await VotingApi.getProcessSummary(processId, pool).catch(
+    const info: ProcessSummary = await VotingApi.getProcessSummary(processId, pool).catch(
       () => null
     );
 
