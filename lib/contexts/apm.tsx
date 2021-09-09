7442fa08e5eb44bc2b830fb70408ef2b7afd41d7
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-09-09 16:37:25 +0200

    minor changes to APM provider

--- lib/contexts/apm.tsx
+++ lib/contexts/apm.tsx
@@ -1,12 +1,12 @@
 import React, { useMemo, useContext, useState } from "react";
-import { init as initApm } from "@elastic/apm-rum";
+import { ApmBase, init as initApm } from "@elastic/apm-rum";
 import { afterFrame } from "@elastic/apm-rum-core";
 import { BUILD } from "../constants/env";
 
 const UseAPMContext = React.createContext(null);
 
 function ApmProvider({ children }) {
-  const [apm, setApm] = useState(() =>
+  const [apm, setApm] = useState<ApmBase>(() =>
     initApm({
       serviceName: "Voice",
       serverUrl: "https://apm-monitoring.aragon.org",
@@ -22,11 +22,11 @@ function ApmProvider({ children }) {
   return <UseAPMContext.Provider value={contextValue}>{children}</UseAPMContext.Provider>;
 }
 
-function useAPM() {
+function useApm() {
   return useContext(UseAPMContext);
 }
 
-function updateAPMContext(apm: any, networkType: string) {
+function updateApmContext(apm: ApmBase, networkType: string) {
   if (apm && networkType) {
     const context = { networkType: networkType };
     apm.addLabels(context);
@@ -34,20 +34,17 @@ function updateAPMContext(apm: any, networkType: string) {
   }
 }
 
-function instrumentApmRoutes(apm: any, routingMod: any) {
-  if (apm && routingMod) {
-    const { instanceId, instancePath, name, status } = routingMod;
-    const path = status ? `${name}/${status}` : `${name}/${instanceId}${instancePath}`;
-
-    const tx = apm.startTransaction(path, "route-change", {
+function instrumentApmRoutes(apm: ApmBase, url: string) {
+  if (apm) {
+    const tx: Transaction = apm.startTransaction(url, "route-change", {
       managed: false,
       canReuse: false,
     });
 
     afterFrame(() => {
-      tx && tx.detectFinish();
+      tx && tx.isFinished();
     });
   }
 }
 
-export { useAPM, ApmProvider, updateAPMContext, instrumentApmRoutes };
+export { useApm, ApmProvider, updateApmContext, instrumentApmRoutes };
