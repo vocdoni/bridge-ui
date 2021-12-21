4f23c7a08d5bf9bafea5285ca68dd6d9b29265fd
p4u <pau@dabax.net>
2021-12-21 10:38:52 +0100

    Revert "cleaned up environment variables"
    This reverts commit 12ad45376257d01afa1f155e4fa9310f6e2b38a3.

--- env-config.js
+++ env-config.js
@@ -11,9 +11,24 @@ module.exports = {
   APP_TITLE:
     "Aragon Voice - the ultimate solution for creating and managing proposals and voting in a decentralized, cost-effective, and secure manner",
   DEVELOPMENT,
+  VOCDONI_ENVIRONMENT: process.env.VOCDONI_ENVIRONMENT || "dev",
+
+  // BLOCKCHAIN
+  ETH_NETWORK_ID: process.env.ETH_NETWORK_ID || "rinkeby",
+  ETH_CHAIN_ID: process.env.ETH_CHAIN_ID ? parseInt(process.env.ETH_CHAIN_ID) : 4,
+  ETHERSCAN_PREFIX: process.env.ETHERSCAN_PREFIX || "https://rinkeby.etherscan.io",
+
+  // VOCHAIN
+  BLOCK_TIME: process.env.BLOCK_TIME || "12",
+
+  // GATEWAYS
+  BOOTNODES_URL: process.env.BOOTNODES_URL || "https://bootnodes.vocdoni.net/gateways.dev.json",
+  SIGNALING_ORACLE_URL:
+    process.env.SIGNALING_ORACLE_URL || "https://signaling-oracle.dev.vocdoni.net/dvote",
 
   // ANALYTICS
   ANALYTICS_KEY: process.env.ANALYTICS_KEY || "1w2W1aAJRMbVRAgHVOIRVVIh1Nc",
+
   FORTMATIC_API_KEY: process.env.FORTMATIC_API_KEY || "pk_test_A5D318B08D001541",
 };
 
