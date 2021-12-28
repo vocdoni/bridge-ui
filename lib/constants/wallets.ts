d5367460c1bfc25380f09d9365461177f0f3a23e
Marc Velmer <marc1987@gmail.com>
2021-12-28 13:47:49 +0100

    Uncomment `Wallet Connect` for testing purposes

--- lib/constants/wallets.ts
+++ lib/constants/wallets.ts
@@ -8,7 +8,7 @@ export interface ConnectorData {
 
 export type SUPPORTED_CONNECTORS =
   | "injected"
-  | /* | "portis" */ "fortmatic" /* | "walletconnect" | "ledger" */;
+  | /* | "portis" */ "fortmatic" | "walletconnect" /* | "ledger" */;
 
 // NOTE: This list of wallets is tentative. So far only Metamask and Fortmatic work
 // consistently. HD wallets seem to cause problems, even when used through Metamask. The
@@ -35,13 +35,14 @@ export const WALLETS: ConnectorData[] = [
   //   connector: "portis",
   //   properties: { dAppId: "4402221a-b7c2-4781-9a0e-b1509dabaa1e" },
   // },
-  // {
-  //   name: "Wallet Connect",
-  //   connector: "walletconnect",
-  //   properties: {
-  //     rpcUrl: `https://${process.env.ETH_NETWORK_ID}.infura.io/v3/b76cba91dc954ceebff27244923224b1`,
-  //   },
-  // },
+  {
+    name: "Wallet Connect",
+    connector: "walletconnect",
+    properties: {
+      rpcUrl: `https://${process.env.ETH_NETWORK_ID}.infura.io/v3/b76cba91dc954ceebff27244923224b1`,
+      chainId: [1, 4],
+    },
+  },
   // {
   //   name: "Ledger",
   //   connector: "ledger",
