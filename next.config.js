df0b564707ea3afa99f77013a7311683cc822fa6
Marc Velmer <marc1987@gmail.com>
2022-01-25 17:24:49 +0100

    Fixed `fs module not found` for NextJS 12 (webpack5)

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
   trailingSlash: true,
   env,
   productionBrowserSourceMaps: true,
+  future: {
+    webpack5: true
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  }
 };
