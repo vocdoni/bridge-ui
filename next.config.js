c120f1fe3b5d9ba5bc53bd0f6fc96c215ed1a2e3
emmdim <emmdim@users.noreply.github.com>
2022-05-03 19:00:31 +0200

    Adds web-pcack compression and duplicate lib removal modules
    Signed-off-by: emmdim <emmdim@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const env = require("./env-config.js");
+var DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const path = require('path');
 
 module.exports = {
   trailingSlash: true,
@@ -7,11 +10,23 @@ module.exports = {
   future: {
     webpack5: true
   },
+  plugins: [new DuplicatePackageCheckerPlugin()],
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
     };
+    config.plugins.push(new DuplicatePackageCheckerPlugin())
+    config.plugins.push(
+        new CompressionPlugin({
+          compressionOptions: { deleteOriginalAssets: true,test: /\.js$|\.css$|\.html$/, algorithm: "gzip" },
+        }),
+    )
+    config.resolve.alias['bn.js'] = path.resolve(
+      __dirname,
+      'node_modules',
+      'bn.js'
+    )
     return config;
   }
 };
